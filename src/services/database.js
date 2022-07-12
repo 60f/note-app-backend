70a6392114b4f1f6053f7cfbf14aea8849f5fe04
60f <96847116+60f@users.noreply.github.com>
2022-07-12 23:03:52 +0200

    Update database credentials

--- src/services/database.js
+++ src/services/database.js
@@ -1,10 +1,10 @@
 const pg = require('pg');
 
 const database = new pg.Client({
-  user: 'ryqaxqoybdmbez',
-  host: 'ec2-54-228-32-29.eu-west-1.compute.amazonaws.com',
-  database: 'd4i7htb4q1ds02',
-  password: '5e0581fce574e8e17bf9e39693950e56b97792909084ca3e7040ac322f79620d',
+  user: 'mvlosrzunwlpxc',
+  host: 'ec2-52-208-164-5.eu-west-1.compute.amazonaws.com',
+  database: 'd132g24aei5268',
+  password: 'de923a5e98f505826d2c5290deaaaf821eeafe9c5c2f5ce4f0789cd0402eb95c',
   port: 5432,
   ssl: {
     rejectUnauthorized: false,
